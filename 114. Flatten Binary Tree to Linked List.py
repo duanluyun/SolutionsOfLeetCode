95e26d8dbe01b875594121a6e610ef07e111b717
LuYun Duan <duanluyun@outlook.com>
2018-08-06 15:37:51 +0800

    Commit 114

--- 114. Flatten Binary Tree to Linked List.py
+++ 114. Flatten Binary Tree to Linked List.py
@@ -6,17 +6,50 @@ class TreeNode(object):
         self.right = None
 
 class Solution(object):
-    def flatten(self):
+    def flatten(self,root):
         """
         :type root: TreeNode
         :rtype: void Do not return anything, modify root in-place instead.
         """
-        a=[1,2,3]
-        b=[4]
-        # a.append(b) //a=[1, 2, 3, [4]]
-        # a+=b  //a=[1,2,3,4]
+        if root==None:
+            return None
+        res=[]
+        res=self.Inorder(root,res)
+        pre=None
+        for node in res:
+            node.left=None
+            if pre!=None:
+                pre.right=node
+            pre=node
 
 
+
+    def Inorder(self,root,res):
+        if root.left==None and root.right==None:
+            res.append(root)
+            return res
+        res.append(root)
+        if root.left:
+            self.Inorder(root.left,res)
+        if root.right:
+            self.Inorder(root.right,res)
+        return res
+
+node1=TreeNode(1)
+node6=TreeNode(6)
+node2=TreeNode(2)
+node5=TreeNode(5)
+node3=TreeNode(3)
+node4=TreeNode(4)
+
+node1.left=node2
+node1.right=node5
+node2.left=node3
+node2.right=node4
+node5.right=node6
 
 S=Solution()
-S.flatten()
+head=S.flatten(node1)
+while head:
+    print(head.val)
+    head=head.right
